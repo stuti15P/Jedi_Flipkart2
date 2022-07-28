60b3fd156248dc144caa8be73b9b988c92f9c47b
Stuti Priyambda <stuti.priyambda@BLRETV-KX96W3W7.local>
2022-07-28 12:19:20 +0530

    After admiDB update

--- CRS-JEDI-Grp4-Flipkart2022-POS/src/com/flipkart/bean/Admin.java
+++ CRS-JEDI-Grp4-Flipkart2022-POS/src/com/flipkart/bean/Admin.java
@@ -1,10 +1,42 @@
 package com.flipkart.bean;
 
+import java.util.Date;
+
 /**
  * @author stuti.priyambda
  *
  */
 
-public class Admin {
+public class Admin extends User{
+	
+	private Date dateOfJoining;
 
+	/**
+	 * Parameterized Constructor
+	 * @param userID
+	 * @param name
+	 * @param role
+	 * @param password
+	 * @param dateOfJoining
+	 */
+	public Admin(String userID, String name, String role, String password, Date dateOfJoining) {
+		super(userID, name, role, password);
+		this.dateOfJoining = dateOfJoining;
+	}
+
+	/**
+	 * Get DOJ
+	 * @return
+	 */
+	public Date getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+	/**
+	 * Set DOJ
+	 * @param dateOfJoining
+	 */
+    public void setDateOfJoining(Date dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
 }
